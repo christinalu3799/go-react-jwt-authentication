38752b5b12f31c704117600eaccb08ccfaf9a2e1
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-12-07 10:39:24 -0500

    created post route and returned data back as JSON

--- controllers/authController.go
+++ controllers/authController.go
@@ -2,6 +2,14 @@ package controllers
 
 import "github.com/gofiber/fiber/v2"
 
-func Hello(c *fiber.Ctx) error {
-	return c.SendString("Hello world!")
+func Register(c *fiber.Ctx) error {
+	// get our data back from the post request
+	var data map[string]string // similar to an array with a string as a key and value
+
+	// error handling
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	return c.JSON(data)
 }
