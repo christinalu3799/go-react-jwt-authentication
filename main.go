3fa468c693d3d32e519b06d8dd16a3e78505ae8f
christina.lu3799@gmail.com <christina.lu3799@gmail.com>
2022-12-12 15:38:42 -0500

    installed cors middleware to send cookie to frontend

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/christinalu3799/go-react-jwt-authentication/database"
 	"github.com/christinalu3799/go-react-jwt-authentication/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -11,6 +12,13 @@ func main() {
 	database.Connect()
 
 	app := fiber.New()
+
+	// user cors to allow frontend to make requests to backend
+	app.Use(cors.New(cors.Config{
+		// this allows our frontend to receive the cookie that server sends
+		// also allows the frontend to send the cookie back
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
